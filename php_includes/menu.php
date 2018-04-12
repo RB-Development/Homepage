a2d70f931bee6bd90b820e3e6a4eefb485aafcdb
Bösche <Bsche@192.168.178.47>
2018-04-12 17:54:52 +0200

    menu

---
+++ php_includes/menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/menu.css"/>
+        <title>Meine eigene Homepage</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <ul>
+            <li><a href="index.php"><i>Home</i></a></li>
+            <li class =menu><a href="Tools.php"><i>Tools</i></a></li>
+            <li class ="menu"><a href="Tutorials.php"><i>Tutorials</i></a></li>
+            <li class ="menu"><a href="Kontakt.php"><i>Kontakt</i></a></li>
+        </ul>
+    </body>
+</html>
